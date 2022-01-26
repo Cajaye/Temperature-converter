a5ac7cc9de12325d11e83449da930ed7ed53a486
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-01-25 23:54:15 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+
+fn main() {
+    //Convert temperatures between Fahrenheit and Celsius.
+
+    let mut input = String::new();
+    let mut mode = String::new();
+
+    loop {
+        println!("Temperature converter!");
+
+        println!("Enter a mode they are: f2c and c2f");
+        io::stdin()
+            .read_line(&mut mode)
+            .expect("Enter a valid input");
+
+        let mode = mode.trim();
+
+        println!("Enter a value");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Enter a valid input");
+
+        let input = match input.trim().parse() {
+            Ok(val) => val,
+            Err(_) => continue,
+        };
+
+        let converted = covert_temp(input, &mode);
+        println!("{} degrees {}", converted, mode);
+        break;
+    }
+}
+
+fn covert_temp(convert: i128, mode: &str) -> i128 {
+    match mode {
+        "f2c" => (convert - 32) * 5 / 9,
+        "c2f" => (convert * 9 / 5) + 32,
+        _ => 0000,
+    }
+}
